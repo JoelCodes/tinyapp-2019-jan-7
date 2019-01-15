b9a18f11caa2a3dd9323b3785bd8244668aa6fbf
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-15 22:46:18 +0000

    Reordered my GET routes from most to least specific. (I think I did it correctly...)

--- express_server.js
+++ express_server.js
@@ -13,9 +13,9 @@ const urlDatabase = [
   { tinyURL: '9sm5xK', fullURL: 'http://www.google.com'},
   ];
 
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase};
-  res.render('urls_index', templateVars);
+// GET routes are ordered from most to least specific
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -23,17 +23,23 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
 
 app.listen(PORT, ()=> {
   console.log(`Example app listening on port ${PORT}!`);
