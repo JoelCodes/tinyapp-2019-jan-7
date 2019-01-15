266e26791552ffaa738ff5b2c5f25f6ad191a54d
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-15 22:41:51 +0000

    Required the body-parser module inside the server file.

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
 
 const urlDatabase = [
   { tinyURL: 'b2xVn2', fullURL: 'http://www.lighthouselabs.ca'},
