b3bbfe726a22e545c61b1266659522037996a3e3
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-16 01:47:31 +0000

    Isolated the idString withouthe first part of the path.

--- express_server.js
+++ express_server.js
@@ -49,10 +49,13 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/u/:shortURL', (req, res) => {
+  const idPath = req.originalUrl;
+  const idString = idPath.split('').slice(3).join('')
   // for (let item of urlDatabase){
   //   console.log(item.fullURL);
   // }
-  console.log(req.originalUrl);
+  console.log(idString);
+  // console.log(req.originalUrl);
   // let longURL = req.params.id;
   // res.redirect(longURL);
 });
