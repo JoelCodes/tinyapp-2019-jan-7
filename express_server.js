ef8c51e9416c0d4dba74ac3872c498c22e1033d6
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-19 00:29:46 +0000

    Adjusted some code to meet airbnb's standards. Not ALL of their standards, mind you...

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
-"use strict";
+'use strict';
 
 // setting up the modules and middleware
 const express = require('express');
+
 const app = express();
 const PORT = 8080;
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const cookieSession = require('cookie-session');
+
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
@@ -17,35 +20,36 @@ app.use(cookieSession({
 app.set('view engine', 'ejs');
 
 const bcrypt = require('bcrypt');
+
 const user1PW = bcrypt.hashSync('cow', 10);
 
 
 // universal variables
 const urlDatabase = [
-  { tinyURL: 'b2xVn2', fullURL: 'http://www.lighthouselabs.ca', owner: "userRandomID"},
-  { tinyURL: '9sm5xK', fullURL: 'http://www.google.com', owner: "user2RandomID"},
-  { tinyURL: 'C2xVn2', fullURL: 'http://www.blighthouselabs.ca', owner: "userRandomID"},
-  ];
+  { tinyURL: 'b2xVn2', fullURL: 'http://www.lighthouselabs.ca', owner: 'userRandomID' },
+  { tinyURL: '9sm5xK', fullURL: 'http://www.google.com', owner: 'user2RandomID' },
+  { tinyURL: 'C2xVn2', fullURL: 'http://www.blighthouselabs.ca', owner: 'userRandomID' },
+];
 
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
     password: user1PW,
   },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "sow"
-  }
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'sow',
+  },
 };
 
 // GET routes (from most to least specific)
 app.get('/urls/new', (req, res) => {
-  let templateVars = {
+  const templateVars = {
     user: getUserObj(req.session.user_id),
   };
-  if (req.session.user_id){
+  if (req.session.user_id) {
     res.render('urls_new', templateVars);
   } else {
     res.redirect('/login');
@@ -54,17 +58,17 @@ app.get('/urls/new', (req, res) => {
 
 app.get('/urls/:id', (req, res) => {
   const user = getUserObj(req.session.user_id);
-  let templateVars = {
+  const templateVars = {
     user: user, // object
     shortURL: req.params.id,
     yourURLs: urlsForUser(req.session.user_id), // array
   };
-  if (urlDatabaseChecker(req.params.id)){
-    if (!req.session.user_id){
+  if (urlDatabaseChecker(req.params.id)) {
+    if (!req.session.user_id) {
       res.send('Try <a href="/login">logging in</a> first.');
-    } else if (!isThisYours(req.params.id, req.session.user_id)){
+    } else if (!isThisYours(req.params.id, req.session.user_id)) {
       res.send('That URL does not belong to you. 😾');
-    } else if (req.session.user_id){
+    } else if (req.session.user_id) {
       res.render('urls_show', templateVars);
     }
   } else {
@@ -73,26 +77,26 @@ app.get('/urls/:id', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  if (req.session.user_id){
-    let templateVars = {
+  if (req.session.user_id) {
+    const templateVars = {
       user: getUserObj(req.session.user_id),
       urls: urlsForUser(req.session.user_id),
     };
     res.render('urls_index', templateVars);
   } else {
-    res.send('You need to <a href="/login">log in</a> to see your shortened URLs.')
+    res.send('You need to <a href="/login">log in</a> to see your shortened URLs.');
   }
 });
 
 app.get('/u/:id', (req, res) => {
   const idString = req.params.id;
   const goHere = [];
-  for (let item of urlDatabase){
-    if (item.tinyURL === idString){
+  for (const item of urlDatabase) {
+    if (item.tinyURL === idString) {
       goHere.push(item.fullURL);
     }
   }
-  if (goHere.length > 0){
+  if (goHere.length > 0) {
     return res.redirect(302, goHere.join(''));
   } else {
     return res.status(404).send('Sorry. That URL is not in our database. 😞 Would you like to <a href="/urls/new">make one</a>?');
@@ -100,7 +104,7 @@ app.get('/u/:id', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  if (req.session.user_id){
+  if (req.session.user_id) {
     res.redirect('/urls');
   } else {
     res.redirect('/login');
@@ -108,16 +112,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  if (!req.session.user_id){
+  if (!req.session.user_id) {
     const templateVars = { user: getUserObj(req.session.user_id) };
     res.render('urls_login', templateVars);
   } else {
     res.redirect('/urls');
   }
-})
+});
 
 app.get('/register', (req, res) => {
-  if (!req.session.user_id){
+  if (!req.session.user_id) {
     const templateVars = {
       user: getUserObj(req.session.user_id),
     };
@@ -125,13 +129,13 @@ app.get('/register', (req, res) => {
   } else {
     res.redirect('/urls');
   }
-})
+});
 
 // POST requests (most to least specific)
 app.post('/urls/:id/delete', (req, res) => {
   const toBeDel = req.params.id;
-  for (let index in urlDatabase){
-    if(urlDatabase[index].tinyURL === toBeDel){
+  for (const index in urlDatabase) {
+    if (urlDatabase[index].tinyURL === toBeDel) {
       urlDatabase.splice(index, 1);
     }
   }
@@ -141,8 +145,8 @@ app.post('/urls/:id/delete', (req, res) => {
 app.post('/urls/:id', (req, res) => {
   const idString = req.params.id;
   const newFull = req.body.newFull;
-  for (let index in urlDatabase){
-    if (urlDatabase[index].tinyURL === idString){
+  for (const index in urlDatabase) {
+    if (urlDatabase[index].tinyURL === idString) {
       urlDatabase[index].fullURL = newFull;
     }
   }
@@ -152,7 +156,7 @@ app.post('/urls/:id', (req, res) => {
 app.post('/urls', (req, res) => {
   const idString = generateRandomString();
   const inputURL = req.body.longURL;
-  urlDatabase.push({tinyURL: idString, fullURL: inputURL, owner: req.session.user_id});
+  urlDatabase.push({ tinyURL: idString, fullURL: inputURL, owner: req.session.user_id });
   // console.log("caw", req.session.user_id);
   // console.log("BAW", );
   res.redirect(`/urls/${idString}`);
@@ -165,14 +169,14 @@ app.post('/login', (req, res) => {
 
   // does hashed input password match hashed stored password?
   const storedPassword = findPassword(submittedEmail);
-  const passwordMatchCheck = bcrypt.compareSync(req.body.password, storedPassword); //boolean
+  const passwordMatchCheck = bcrypt.compareSync(req.body.password, storedPassword); // boolean
 
   const userID = findUserID(submittedEmail);
 
-  if (emailMatchCheck && passwordMatchCheck){
+  if (emailMatchCheck && passwordMatchCheck) {
     req.session.user_id = users[userID].id;
     res.redirect('/urls');
-  } else if (!emailMatchCheck || !passwordMatchCheck){
+  } else if (!emailMatchCheck || !passwordMatchCheck) {
     res.send('Sorry, pal. Your email or password do not match. Try <a href="/login">logging in</a> again or <a href="/register">register an account</a>. 🚶');
   }
 });
@@ -183,12 +187,12 @@ app.post('/logout', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  for (let user in users){
-    if (users[user].email === req.body.email){
+  for (const user in users) {
+    if (users[user].email === req.body.email) {
       res.status(400).send('Email is already in the system. Try <a href="/login">logging in</a>.');
     }
   }
-  if (req.body.email && req.body.password){
+  if (req.body.email && req.body.password) {
     req.session.user_id = generateRandomString();
     users[req.session.user_id] = {
       id: req.session.user_id,
@@ -201,16 +205,16 @@ app.post('/register', (req, res) => {
   }
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
   console.log(`Shmoo's tiny app listening on port ${PORT}!`);
 });
 
 // HELPER functions
 function generateRandomString() {
-  const everyAlphaNum = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`;
+  const everyAlphaNum = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
   const output = [];
-  for (let i = 0; i < 6; i++){
-    let randomIndex = Math.floor(Math.random() * Math.floor(61));
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * Math.floor(61));
     output.push(everyAlphaNum[randomIndex]);
   }
   return output.join('');
@@ -221,10 +225,10 @@ function getUserObj(theCookie) {
   return userObj;
 }
 
-function findUserID(email){
+function findUserID(email) {
   let output;
-  for (let user in users){
-    if (users[user].email === email){
+  for (const user in users) {
+    if (users[user].email === email) {
       output = users[user].id;
       return output;
     }
@@ -232,43 +236,43 @@ function findUserID(email){
 }
 
 function urlsForUser(id) {
-  let ownedURLs = [];
+  const ownedURLs = [];
   urlDatabase.forEach((url) => {
-    if (url.owner === id){
+    if (url.owner === id) {
       ownedURLs.push(url);
     }
   });
   return ownedURLs;
 }
 
-function emailMatchChecker(email){
-  for (let user in users){
-    if (users[user].email === email){
+function emailMatchChecker(email) {
+  for (const user in users) {
+    if (users[user].email === email) {
       return true;
     }
   }
 }
 
-function findPassword(email){
-  for (let user in users){
-    if (users[user].email === email){
+function findPassword(email) {
+  for (const user in users) {
+    if (users[user].email === email) {
       return users[user].password;
     }
   }
 }
 
-function urlDatabaseChecker(shortURL){
-  for (let url of urlDatabase){
-    if (url.tinyURL === shortURL){
+function urlDatabaseChecker(shortURL) {
+  for (const url of urlDatabase) {
+    if (url.tinyURL === shortURL) {
       return true;
     }
   }
 }
 
-function isThisYours(shortURL, userID){
-  for (let url of urlDatabase){
-    if (url.tinyURL === shortURL){
-      if (url.owner === userID){
+function isThisYours(shortURL, userID) {
+  for (const url of urlDatabase) {
+    if (url.tinyURL === shortURL) {
+      if (url.owner === userID) {
         return true;
       }
     }
